9d456b3125bc3372c696d931fdc613ea02c0200d
giacomoaccursi <giacomo.accursi@gmail.com>
2023-11-10 14:56:14 +0100

    feat: create content interface

---
+++ src/main/kotlin/entity/Content.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023. Accursi Giacomo
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity
+
+/**
+ * A container of values.
+ */
+interface Content {
+    /**
+     * The value.
+     */
+    var value: Int
+}
