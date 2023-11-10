6cb8f53f113c4684cfd69f1c426a547ad50fecb3
giacomoaccursi <giacomo.accursi@gmail.com>
2023-11-10 16:28:48 +0100

    feat: create engine interface

---
+++ src/main/kotlin/control/Engine.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023. Accursi Giacomo
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package control
+
+/**
+ * The engine of the simulation.
+ */
+interface Engine {
+    /**
+     * Start the engine.
+     */
+    fun start()
+}
