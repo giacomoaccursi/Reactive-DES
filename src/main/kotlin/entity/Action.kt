6b64a7c6c33d9ba5508505cefe040c61a04f87b3
giacomoaccursi <giacomo.accursi@gmail.com>
2023-11-10 15:37:02 +0100

    feat: create action interface

---
+++ src/main/kotlin/entity/Action.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023. Accursi Giacomo
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity
+
+/**
+ * Represent an action.
+ */
+interface Action {
+    /**
+     * Executes the action.
+     */
+    fun execute()
+}
