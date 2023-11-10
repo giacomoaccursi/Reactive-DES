c43e9fdcc1a03a705bf611b76af3a7f254fad7fa
giacomoaccursi <giacomo.accursi@gmail.com>
2023-11-10 15:37:46 +0100

    feat: create time equation

---
+++ src/main/kotlin/entity/TimeEquation.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023. Accursi Giacomo
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity
+
+import org.apache.commons.math3.distribution.ExponentialDistribution
+
+/**
+ * Represent a time equation.
+ */
+class TimeEquation {
+    /**
+     * Only for test purposes it has been initialized as exponential distribution.
+     */
+    private val distribution = ExponentialDistribution(1.0)
+    private lateinit var tau: Time
+
+    /**
+     * Update the time equation.
+     * @param currentTime the current time of the environment.
+     */
+    fun update(currentTime: Time) {
+        val step = distribution.sample()
+        tau = DoubleTime(currentTime.toDouble() + step)
+    }
+
+    /**
+     * The next time at which this event will occur.
+     */
+    fun getNextOccurrence(): Time = tau
+}
