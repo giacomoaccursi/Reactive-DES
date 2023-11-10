75d55f38c541903ecc70495f85c2eca3d7fb8fd2
giacomoaccursi <giacomo.accursi@gmail.com>
2023-11-10 15:45:57 +0100

    chore: add simple implementation of a content

---
+++ src/main/kotlin/entity/ContentImpl.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023. Accursi Giacomo
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity
+
+/**
+ * Simple implementation of a content.
+ */
+class ContentImpl(override var value: Int) : Content
