4b20f1162c2750b288bb6b9609962b0687200d77
giacomoaccursi <giacomo.accursi@gmail.com>
2023-11-16 14:29:13 +0100

    chore: change project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.0"
 }
 
-rootProject.name = "kotlin-template-project"
+rootProject.name = "DES-Reactive-Engine"
 
 gradleEnterprise {
     buildScan {
