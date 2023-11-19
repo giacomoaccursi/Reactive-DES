efec2336735fca75516a9484403eb3caa9a6a802
giacomoaccursi <giacomo.accursi@gmail.com>
2023-11-19 16:47:28 +0100

    chore: create simple neighborhood implementation

--- src/main/kotlin/entity/SimpleNeighborhood.kt
+++ src/main/kotlin/entity/SimpleNeighborhood.kt
@@ -8,14 +8,38 @@
 
 package entity
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.util.concurrent.CountDownLatch
+
 /**
  * A simple implementation of a neighborhood.
  */
 class SimpleNeighborhood(
     private val center: Node,
+    private val environment: Environment,
+    private val linkingRule: LinkingRule,
 ) : Neighborhood {
 
     private var neighbors: Set<Node> = setOf()
+    override fun startObservingNodes(scope: CoroutineScope): CountDownLatch {
+        val latch = CountDownLatch(environment.nodes.size)
+        environment.nodes.forEach { node ->
+            latch.countDown()
+            scope.launch {
+                node.observeNodePosition().collect {
+                    if (node != center) {
+                        if (linkingRule.isNeighbor(center, node)) {
+                            neighbors += node
+                        } else {
+                            neighbors -= node
+                        }
+                    }
+                }
+            }
+        }
+        return latch
+    }
 
     override fun getCenter() = center
     override fun getNeighbors() = neighbors
