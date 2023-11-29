8e9b8a0630070575cf9070f5d6a9726beb5da4dc
giacomoaccursi <giacomo.accursi@gmail.com>
2023-11-29 18:07:47 +0100

    chore: neighborhood observe nodes position from the environment

--- src/main/kotlin/entity/Neighborhood.kt
+++ src/main/kotlin/entity/Neighborhood.kt
@@ -8,18 +8,10 @@
 
 package entity
 
-import kotlinx.coroutines.CoroutineScope
-
 /**
  * Interface for a neighborhood.
  */
 interface Neighborhood {
-
-    /**
-     * Starts to observing environment nodes.
-     * @param scope the coroutine scope.
-     */
-    suspend fun startObservingNodes(scope: CoroutineScope)
 
     /**
      * @return the center of the neighborhood.
@@ -30,16 +22,4 @@ interface Neighborhood {
      * @return the neighbors of the neighborhood.
      */
     fun getNeighbors(): Set<Node>
-
-    /**
-     * Add new neighbor.
-     * @param node the node to add.
-     */
-    fun addNeighbor(node: Node)
-
-    /**
-     * Remove a node from neighbors.
-     * @param node the node to remove.
-     */
-    fun removeNeighbor(node: Node)
 }
--- src/main/kotlin/entity/SimpleNeighborhood.kt
+++ src/main/kotlin/entity/SimpleNeighborhood.kt
@@ -10,7 +10,6 @@ package entity
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
@@ -21,72 +20,24 @@ class SimpleNeighborhood(
     private val center: Node,
     private val environment: Environment,
     private val linkingRule: LinkingRule,
-    private val nodeChangeFlow: MutableSharedFlow<NodeEvent>,
-    coroutineContext: CoroutineContext = Dispatchers.Default,
+    private val coroutineContext: CoroutineContext = Dispatchers.Default,
 ) : Neighborhood {
 
     init {
+        startObservingNodes()
+    }
+
+    private var neighbors: Set<Node> = emptySet()
+
+    private fun startObservingNodes() {
         CoroutineScope(coroutineContext).launch {
-            startObservingNodes(this)
-        }
-    }
-
-    private var neighbors: Set<Node> =
-        linkingRule.computeNeighbors(center, environment)
-
-    override suspend fun startObservingNodes(scope: CoroutineScope) {
-        scope.launch {
-            nodeChangeFlow.collect { event ->
-                when (event) {
-                    is NodeEvent.NodeAdded -> onNodeAdded(event.node)
-                    is NodeEvent.NodeMoved -> onNodeMoved(event.node)
-                    is NodeEvent.NodeRemoved -> onNodeRemoved(event.node)
-                }
+            environment.nodesToPosition.collect {
+                neighbors = linkingRule.computeNeighbors(center, environment)
             }
-        }
-    }
-
-    private fun onNodeMoved(node: Node) {
-        if (node != center) {
-            if (linkingRule.isNeighbor(center, node, environment)) {
-                if (!neighbors.contains(node)) {
-                    neighbors += node
-                }
-            } else {
-                if (neighbors.contains(node)) {
-                    neighbors -= node
-                }
-            }
-        } else {
-            neighbors = linkingRule.computeNeighbors(center, environment)
-        }
-    }
-
-    private fun onNodeAdded(node: Node) {
-        if (node != center) {
-            if (linkingRule.isNeighbor(center, node, environment)) {
-                if (!neighbors.contains(node)) {
-                    neighbors += node
-                }
-            }
-        }
-    }
-
-    private fun onNodeRemoved(node: Node) {
-        if (neighbors.contains(node)) {
-            neighbors -= node
         }
     }
 
     override fun getCenter() = center
 
     override fun getNeighbors() = neighbors
-
-    override fun addNeighbor(node: Node) {
-        neighbors += node
-    }
-
-    override fun removeNeighbor(node: Node) {
-        neighbors -= node
-    }
 }
