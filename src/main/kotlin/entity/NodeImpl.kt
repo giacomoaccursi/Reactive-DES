d6409879bed850eb5a2d190e962fa98f90fdfa88
giacomoaccursi <giacomo.accursi@gmail.com>
2023-12-01 10:31:38 +0100

    chore: make observable the events and contents of a node

--- src/main/kotlin/entity/NodeImpl.kt
+++ src/main/kotlin/entity/NodeImpl.kt
@@ -8,21 +8,32 @@
 
 package entity
 
+import kotlinx.coroutines.flow.MutableStateFlow
+
 /**
  * An implementation of node.
  */
 class NodeImpl(
     override val id: Int,
-    override val events: ArrayList<Event> = ArrayList(),
-    override val contents: ArrayList<Content> = ArrayList(),
 ) : Node {
 
+    override val events: MutableStateFlow<List<Event>> = MutableStateFlow(emptyList())
+    override val contents: MutableStateFlow<List<Content>> = MutableStateFlow(emptyList())
+
     override fun addEvent(event: Event) {
-        events.add(event)
+        events.value += event
     }
 
     override fun removeEvent(event: Event) {
-        events.remove(event)
+        events.value -= event
+    }
+
+    override fun addContent(content: Content) {
+        contents.value += content
+    }
+
+    override fun removeContent(content: Content) {
+        contents.value -= content
     }
 
     override fun equals(other: Any?): Boolean {
