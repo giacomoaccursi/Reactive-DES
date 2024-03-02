bc96844037e20ea90e8d7d4813ac28affa1839e9
giacomoaccursi <giacomo.accursi@gmail.com>
2024-03-02 16:40:37 +0100

    chore: implement equals and hashcode in eventImpl

--- src/main/kotlin/entity/EventImpl.kt
+++ src/main/kotlin/entity/EventImpl.kt
@@ -145,4 +145,16 @@ class EventImpl(
             }
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as EventImpl
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id
+    }
 }
